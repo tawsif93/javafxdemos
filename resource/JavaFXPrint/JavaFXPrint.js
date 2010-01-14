1c31b03c75f7ac71cff9c1a84e18a4d1cee2c9ba
javafx <javafx@219.64.187.88.ahd.dialup.vsnl.net.in>
2010-01-14 14:10:47 +0530

    Update size of Applet

--- resource/JavaFXPrint/JavaFXPrint.js
+++ resource/JavaFXPrint/JavaFXPrint.js
@@ -1,3 +1,3 @@
 function showJavaFXPrintApplet() {
-    document.getElementById("JavaFXPrintApplet").innerHTML = "<IFRAME alt=JavaFXPrint scrolling=no frameborder=0 src=http://javafxdemos.googlecode.com/hg/resource/JavaFXPrint/JavaFXPrint.html width=360 height=520 ></IFRAME>";
+    document.getElementById("JavaFXPrintApplet").innerHTML = "<IFRAME alt=JavaFXPrint scrolling=no frameborder=0 src=http://javafxdemos.googlecode.com/hg/resource/JavaFXPrint/JavaFXPrint.html width=325 height=325 ></IFRAME>";
 }
