3fc58ddcb7b4a618ef4566a53e10006719597142
javafx <javafx@219.64.187.88.ahd.dialup.vsnl.net.in>
2010-01-14 13:41:26 +0530

    JavaFXPrint - resource

---
+++ resource/JavaFXPrint/JavaFXPrint.js
@@ -0,0 +1,3 @@
+function showJavaFXPrintApplet() {
+    document.getElementById("JavaFXPrintApplet").innerHTML = "<IFRAME alt=JavaFXPrint scrolling=no frameborder=0 src=http://javafxdemos.googlecode.com/hg/resource/JavaFXPrint/JavaFXPrint.html width=360 height=520 ></IFRAME>";
+}
