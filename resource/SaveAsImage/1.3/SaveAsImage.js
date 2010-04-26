4b98de14cec061b5fc44ea069c7680ff7c247975
Rakesh Menon <Rakesh Menon@localhost>
2010-04-26 16:56:42 +0530

    SaveAsImage.js for JavaFX 1.3

---
+++ resource/SaveAsImage/1.3/SaveAsImage.js
@@ -0,0 +1,3 @@
+function showSaveAsImageApplet() {
+    document.getElementById("SaveAsImageApplet").innerHTML = "<IFRAME alt=SaveAsImage scrolling=no frameborder=0 src=http://javafxdemos.googlecode.com/hg/resource/SaveAsImage/1.3/SaveAsImage.html width=355 height=267 ></IFRAME>";
+}
