b99e68a28feeab6c1bfc0d565d79c376766b0f5d
Rakesh Menon <Rakesh Menon@localhost>
2010-05-10 08:52:54 +0530

    Add FileBrowser.js

---
+++ resource/FileBrowser/FileBrowser.js
@@ -0,0 +1,3 @@
+function showFileBrowserApplet() {
+    document.getElementById("FileBrowserApplet").innerHTML = "<IFRAME alt=FileBrowser scrolling=no frameborder=0 src=http://javafxdemos.googlecode.com/hg/resource/FileBrowser/FileBrowser.html width=330 height=330 ></IFRAME>";
+}
